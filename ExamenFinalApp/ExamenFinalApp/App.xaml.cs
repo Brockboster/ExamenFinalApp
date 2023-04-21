cb593b8a1278a4e3fafbac591a938febfa706505
Admin <Admin@DESKTOP-ULMSP3L>
2023-04-21 16:03:09 -0600

    Add project files.

---
+++ ExamenFinalApp/ExamenFinalApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ExamenFinalApp.Services;
+using ExamenFinalApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamenFinalApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            
+            MainPage = new NavigationPage(new BienvenidaPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ExamenFinalApp/ExamenFinalApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using ExamenFinalApp.ViewModels;
+using Xamarin.Forms;
+
+namespace ExamenFinalApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
